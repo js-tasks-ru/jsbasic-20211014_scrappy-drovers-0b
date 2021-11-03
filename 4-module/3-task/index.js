a41d80d7810efbf7e6f02880a196065e93bd207e
Yuri Belov <scrappy_drovers_0b@icloud.com>
2021-11-04 00:23:45 +0300

    module-4 tasks 1 to 3 completed

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,45 @@
 function highlight(table) {
-  // ваш код...
+  const keyWords = Array.from(table.rows[0].children);
+  const statusCellIndex = getCellIndex(keyWords, "Status");
+  const genderCellIndex = getCellIndex(keyWords, "Gender");
+  const ageCellIndex = getCellIndex(keyWords, "Age");
+  for (const row of table.rows) {
+    const statusCell = row.cells[statusCellIndex];
+    const genderCell = row.cells[genderCellIndex];
+    const ageCell = row.cells[ageCellIndex];
+    const availability = getAvailability(statusCell);
+    const gender = getGender(genderCell);
+    if (availability) {
+      row.classList.add(availability);
+    } else {
+      row.setAttribute("hidden", true);
+    }
+    row.classList.add(gender);
+    if (+ageCell.innerHTML < 18) {
+      row.style.textDecoration = "line-through";
+    }
+  }
 }
+
+function getCellIndex(keyWords, textNode) {
+  return keyWords.find(
+    (node) => node.innerHTML === textNode
+  ).cellIndex;
+};
+
+function getAvailability(cell) {
+  if (cell.hasAttribute("data-available")) {
+    if (cell.dataset.available === "true") {
+      return "available";
+    }
+    return "unavailable";
+  }
+  return "";
+};
+
+function getGender(cell) {
+  if (cell.innerHTML === "m") {
+    return "male";
+  }
+  return "female";
+};
