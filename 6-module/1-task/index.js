14efd1926449e91a3c63ed0bc5afa245be460c82
Yuri Belov <scrappy_drovers_0b@icloud.com>
2021-11-14 16:29:51 +0300

    completed 1-2, 3 in progress

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,38 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.render();
+    let [...curRows] = this.rows.map(
+      ({ name, age, salary, city }) =>
+        `<tr>
+        <td>${name}</td>
+        <td>${age}</td>
+        <td>${salary}</td>
+        <td>${city}</td>
+        <td>
+          <button onclick="this.parentElement.parentElement.remove();">X</button>
+        </td>
+      </tr>`
+    );
+    let tbody = this.elem.querySelector("tbody");
+    tbody.innerHTML = `${curRows.join("")}`;
+  }
+
+  render() {
+    this.elem = document.createElement("div");
+    this.elem.innerHTML = `<table>
+    <thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+    </tbody>
+    </table>`;
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,86 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this.render();
+    let slideElements = this.elem.querySelectorAll(".carousel__slide");
+    slideElements.forEach((element) => {
+      let button = element.querySelector(".carousel__button");
+      button.onclick = function () {
+        button.dispatchEvent(
+          new CustomEvent("product-add", {
+            detail: element.dataset.id,
+            bubbles: true,
+          })
+        );
+      };
+    });
+    this.initCarousel();
+  }
+
+  initCarousel() {
+    let checkExist = setInterval(function () {
+      let slidesContainer = document.querySelector(".carousel__inner");
+      if (slidesContainer.children.length > 0) {
+        let slideWidth = slidesContainer.offsetWidth;
+        let numberOfSlides = slidesContainer.children.length;
+        let buttonRight = document.querySelector(".carousel__arrow_right");
+        let buttonLeft = document.querySelector(".carousel__arrow_left");
+        let currentWidth = 0;
+        let slidesCounter = 1;
+        if (slidesCounter === 1) {
+          buttonLeft.style.display = "none";
+        }
+        buttonRight.onclick = function () {
+          buttonLeft.style.display = "";
+          slidesCounter += 1;
+          if (slidesCounter === numberOfSlides) {
+            buttonRight.style.display = "none";
+          }
+          slidesContainer.style.transform = `translateX(${(currentWidth -=
+            slideWidth)}px)`;
+        };
+        buttonLeft.onclick = function () {
+          slidesCounter -= 1;
+          if (slidesCounter === 1) {
+            buttonLeft.style.display = "none";
+            buttonRight.style.display = "";
+          }
+          slidesContainer.style.transform = `translateX(${(currentWidth +=
+            slideWidth)}px)`;
+        };
+        clearInterval(checkExist);
+      }
+    }, 100);
+  }
+
+  render() {
+    let composedSlides = this.slides.map((slide) => {
+      return `<div class="carousel__slide" data-id="${slide.id}">
+        <img src="/assets/images/carousel/${
+          slide.image
+        }" class="carousel__img" alt="slide">
+        <div class="carousel__caption">
+          <span class="carousel__price">€${slide.price.toFixed(2)}</span>
+          <div class="carousel__title">${slide.name}</div>
+          <button type="button" class="carousel__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>`;
+    });
+
+    this.elem = createElement(`<div class="carousel">
+    <div class="carousel__arrow carousel__arrow_right">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </div>
+    <div class="carousel__arrow carousel__arrow_left">
+      <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+    </div>
+    <div class="carousel__inner">
+    ${composedSlides.join("")}
+      </div>
+      </div>`);
   }
 }
