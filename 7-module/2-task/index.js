255fcc29974a40cd15cf8666dcff792c52f4b43a
Yuri Belov <scrappy_drovers_0b@icloud.com>
2021-11-22 00:02:19 +0300

    module-7 tasks 1 to 4 completed

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,6 +1,74 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class Modal {
   constructor() {
+    this.render();
+    this.setTitle();
+    this.setBody();
+    this.open();
+
+    this.listener = (event) => {
+      if (event.code === "Escape") {
+        this.close();
+      }
+    };
+    document.addEventListener("keydown", this.listener);
+  }
+
+  render() {
+    this.modal = document.createElement("div");
+    this.modal.classList.add("modal");
+    this.modal.innerHTML = `
+    <div class="modal__overlay"></div>
+    <div class="modal__inner">
+      <div class="modal__header">
+        <button type="button" class="modal__close">
+          <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+        </button>
+        <h3 class="modal__title">
+          Вот сюда нужно добавлять заголовок
+        </h3>
+      </div>
+      <div class="modal__body">
+        A сюда нужно добавлять содержимое тела модального окна
+      </div>
+    </div>
+    `;
+  }
+
+  open() {
+    document.body.append(this.modal);
+    document.body.classList.add("is-modal-open");
+    let closeButton = this.modal.querySelector(".modal__close");
+    closeButton.addEventListener("click", () => {
+      this.onClick();
+    });
+  }
+
+  setTitle(title) {
+    let currentTitle = this.modal.querySelector(".modal__title");
+    currentTitle.innerHTML = title;
+  }
+
+  setBody(node) {
+    let content = this.modal.querySelector(".modal__body");
+    content.innerHTML = "";
+    content.append(node);
+  }
+
+  close() {
+    document.body.innerHTML = "";
+    document.body.classList.remove("is-modal-open");
+    this.removeListener();
+  }
+
+  onClick() {
+    this.close();
+  }
+
+  removeListener() {
+    if (!document.body.classList.contains("is-modal-open")) {
+      document.removeEventListener("keydown", this.listener);
+    }
   }
 }
--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -1,4 +1,116 @@
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.steps = steps;
+    this.value = value;
+    this.render();
+    let sliderRootElement = this.elem.getRootNode();
+    sliderRootElement.addEventListener("click", (e) => {
+      this.onClick(e);
+    });
+    sliderRootElement.addEventListener("pointerdown", (e) => {
+      this.onPointerDown(e);
+    });
+    sliderRootElement.addEventListener("pointermove", (e) => {
+      sliderRootElement.classList.add("slider_dragging");
+      this.onPointerMove(e);
+    });
+    sliderRootElement.addEventListener("pointerup", () => {
+      this.onPointerUp();
+      sliderRootElement.classList.remove("slider_dragging");
+    });
+    sliderRootElement.ondragstart = () => false;
   }
+
+  render() {
+    this.elem = document.createElement("div");
+    this.elem.classList.add("slider");
+    let spans = [...Array(this.steps).keys()].map((step, index) => {
+      if (index === 0) {
+        return `<span class="slider__step-active"></span>`;
+      }
+      return `<span></span>`;
+    });
+    this.elem.innerHTML = `
+    <div class="slider__thumb" style="left: 25%;">
+    <span class="slider__value">1</span>
+    </div>
+    <div class="slider__progress" style="width: 25%;"></div>
+    <div class="slider__steps">
+      ${spans.join("")}
+    </div>
+  `;
+  }
+
+  onPointerDown = (event) => {
+    event.preventDefault();
+  };
+
+  onClick = (event) => {
+    let leftRelative = this.getleftRelative(event, this.elem);
+    this.value = this.calculatePosition(leftRelative);
+    let valuePercents = this.calculatePercentage(this.value);
+    this.paint(this.value, valuePercents);
+  };
+
+  onPointerMove = (event) => {
+    let leftRelative = this.getleftRelative(event, this.elem);
+
+    if (leftRelative < 0) {
+      leftRelative = 0;
+    }
+
+    if (leftRelative > 1) {
+      leftRelative = 1;
+    }
+
+    let leftPercents = leftRelative * 100;
+    this.value = this.calculatePosition(leftRelative);
+    this.paint(this.value, leftPercents);
+  };
+
+  onPointerUp = () => {
+    this.elem.removeEventListener("pointerup", this.onPointerUp);
+    this.elem.removeEventListener("pointermove", this.onPointerMove);
+    this.generateEvent(this.value);
+  };
+
+  calculatePosition = (leftRelative) => {
+    let segments = this.steps - 1;
+    let approximateValue = leftRelative * segments;
+    return Math.round(approximateValue);
+  };
+
+  calculatePercentage = (value) => {
+    return (value / (this.steps - 1)) * 100;
+  };
+
+  paint = (value, valuePercents) => {
+    let thumb = this.elem.querySelector(".slider__thumb");
+    let progress = this.elem.querySelector(".slider__progress");
+    this.changeActive(value);
+    thumb.style.left = `${valuePercents}%`;
+    progress.style.width = `${valuePercents}%`;
+  };
+
+  changeActive = (value) => {
+    let sliderValue = this.elem.querySelector(".slider__value");
+    let spanElements = this.elem.querySelector(".slider__steps").children;
+    sliderValue.innerHTML = value;
+    Array.from(spanElements).forEach((span) =>
+      span.classList.remove("slider__step-active")
+    );
+    spanElements[value].classList.add("slider__step-active");
+    this.generateEvent(value);
+  };
+
+  generateEvent = (value) => {
+    let sliderChange = new CustomEvent("slider-change", {
+      detail: value,
+      bubbles: true,
+    });
+    this.elem.dispatchEvent(sliderChange);
+  };
+
+  getleftRelative = (event, elem) =>
+    (event.clientX - elem.getBoundingClientRect().left) / elem.offsetWidth;
 }
