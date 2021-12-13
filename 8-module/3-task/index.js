7c3eded1a8abcb89c86471e2d598ccefda993799
Yuri Belov <scrappy_drovers_0b@icloud.com>
2021-12-14 01:23:17 +0300

    module-8 tasks 1 to 4 completed

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,50 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if (product) {
+      let cartItem = this.cartItems.filter(
+        (item) => item.product.id === product.id
+      )[0];
+      let count = 1;
+      if (cartItem) {
+        cartItem.count += count;
+      } else {
+        cartItem = { product, count };
+        this.cartItems = [...this.cartItems, cartItem];
+      }
+      this.onProductUpdate(cartItem);
+    }
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let cartItem = this.cartItems.filter(
+      (item) => item.product.id === productId
+    )[0];
+    if (cartItem) {
+      cartItem.count += amount;
+      if (cartItem.count === 0) {
+        this.cartItems = this.cartItems.filter(
+          (item) => item.product.id !== productId
+        );
+      }
+      this.onProductUpdate(cartItem);
+    }
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length === 0;
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce(function (sum, cartItem) {
+      return sum + cartItem.count;
+    }, 0);
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce(function (sum, cartItem) {
+      return sum + cartItem.count * cartItem.product.price;
+    }, 0);
   }
 
   onProductUpdate(cartItem) {
@@ -31,4 +58,3 @@ export default class Cart {
     this.cartIcon.update(this);
   }
 }
-
